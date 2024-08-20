64b4e136fb6a247c4e20005c45da80a30d95d9ed
Aakash-prvt <akashpremiums03@gmail.com>
2024-08-20 21:40:12 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -19,7 +19,7 @@ async def is_subscribed(filter, client, update):
 
     member_status = ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.MEMBER
 
-    for channel_id in [FORCE_SUB_CHANNEL_1, FORCE_SUB_CHANNEL_2, FORCE_SUB_CHANNEL_3, FORCE_SUB_CHANNEL_4]:
+    for channel_id in [FORCE_SUB_CHANNEL, FORCE_SUB_CHANNEL2, FORCE_SUB_CHANNEL3, FORCE_SUB_CHANNEL4]:
         if not channel_id:
             continue
 
